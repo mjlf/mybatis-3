d8ac038e80946a8670ae73a2182262ed4315dd34
mjlf <mjlfto@163.com>
2020-05-14 11:48:12 +0800

    源码阅读， executor基本查看

---
+++ src/main/java/org/apache/ibatis/executor/Executor.java
@@ -0,0 +1,80 @@
+/**
+ *    Copyright 2009-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.executor;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.cursor.Cursor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.transaction.Transaction;
+
+/**
+ * @author Clinton Begin
+ * 这个地方放用到装饰者模式
+ * if (ExecutorType.BATCH == executorType) {
+ *       executor = new BatchExecutor(this, transaction);
+ *     } else if (ExecutorType.REUSE == executorType) {
+ *       executor = new ReuseExecutor(this, transaction);
+ *     } else {
+ *       executor = new SimpleExecutor(this, transaction);
+ *     }
+ * 他的具体实现类分别有SimpleExecutor, BatchExecutor, ResueExecutor
+ * 而在每个具体实现类种又包含了Executor对象， 典型的装饰者模式
+ *
+ * 这个接口定义的有关 Sql 执行的方法 以及 事务提交以及回滚的方法
+ * 还有和缓冲，连接关闭相关的的操作方法
+ */
+public interface Executor {
+
+  ResultHandler NO_RESULT_HANDLER = null;
+
+  int update(MappedStatement ms, Object parameter) throws SQLException;
+
+  <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, CacheKey cacheKey, BoundSql boundSql) throws SQLException;
+
+  <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException;
+
+  <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException;
+
+  List<BatchResult> flushStatements() throws SQLException;
+
+  void commit(boolean required) throws SQLException;
+
+  void rollback(boolean required) throws SQLException;
+
+  CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql);
+
+  boolean isCached(MappedStatement ms, CacheKey key);
+
+  void clearLocalCache();
+
+  void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType);
+
+  Transaction getTransaction();
+
+  void close(boolean forceRollback);
+
+  boolean isClosed();
+
+  void setExecutorWrapper(Executor executor);
+
+}
---
+++ src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -0,0 +1,113 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.session;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Properties;
+
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.exceptions.ExceptionFactory;
+import org.apache.ibatis.executor.ErrorContext;
+import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
+
+/**
+ * Builds {@link SqlSession} instances.
+ *
+ * @author Clinton Begin
+ */
+public class SqlSessionFactoryBuilder {
+
+  public SqlSessionFactory build(Reader reader) {
+    return build(reader, null, null);
+  }
+
+  public SqlSessionFactory build(Reader reader, String environment) {
+    return build(reader, environment, null);
+  }
+
+  public SqlSessionFactory build(Reader reader, Properties properties) {
+    return build(reader, null, properties);
+  }
+
+  public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
+    try {
+      XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+      return build(parser.parse());
+    } catch (Exception e) {
+      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+    } finally {
+      ErrorContext.instance().reset();
+      try {
+        reader.close();
+      } catch (IOException e) {
+        // Intentionally ignore. Prefer previous error.
+      }
+    }
+  }
+
+  public SqlSessionFactory build(InputStream inputStream) {
+    return build(inputStream, null, null);
+  }
+
+  public SqlSessionFactory build(InputStream inputStream, String environment) {
+    return build(inputStream, environment, null);
+  }
+
+  public SqlSessionFactory build(InputStream inputStream, Properties properties) {
+    return build(inputStream, null, properties);
+  }
+
+  /**
+   * 这个方法是最终用来生成Configuration配置类的build方法，我们最终的目的是构建一个SqlSessionFactory对象出来
+   *
+   * 这里是通过xml配置流， 环境， 配置属性构建Factory
+   * @param inputStream
+   * @param environment
+   * @param properties
+   * @return
+   */
+  public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
+    try {
+      //xml配置解析构建器
+      XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
+
+      //parser.parse()最后会生成一个configuation对象， 这个是mybatis配置的核心类对象
+      return build(parser.parse());
+    } catch (Exception e) {
+      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+    } finally {
+      ErrorContext.instance().reset();
+      try {
+        inputStream.close();
+      } catch (IOException e) {
+        // Intentionally ignore. Prefer previous error.
+      }
+    }
+  }
+
+  /**
+   * 最后通过Configuration构建SqlSessionFactory
+   * 不管是基于xml配置类还是通过 类配置的方法， 最终都会执行这个方法
+   * @param config
+   * @return
+   */
+  public SqlSessionFactory build(Configuration config) {
+    return new DefaultSqlSessionFactory(config);
+  }
+
+}
